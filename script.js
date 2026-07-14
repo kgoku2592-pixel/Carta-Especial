d56ee66ec31b5edb5cac3a407ab73a81e463c9b4
kgoku2592-pixel <kgoku2592@gmail.com>
2026-07-14 00:46:38 -0400

    Refactor envelope click event to add classes

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const envelope = document.getElementById("envelope");
+const carta = document.querySelector(".carta");
+const texto = document.querySelector(".clique");
 
 envelope.addEventListener("click", () => {
-    envelope.classList.toggle("aberto");
-});
+
+    envelope.classList.add("abrir");
+
+    texto.style.display = "none";
+
+    carta.style.display = "block";
+
+    setTimeout(() => {
+        carta.classList.add("mostrar");
+    }, 150);
+
 });
