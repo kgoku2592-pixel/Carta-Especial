aee28f145b51f6bd2702cfd38ed0a3ed0a0bc4fb
kgoku2592-pixel <kgoku2592@gmail.com>
2026-07-14 00:24:26 -0400

    Simplify click event handling for envelope
    Refactor envelope click event to toggle 'aberto' class.

--- script.js
+++ script.js
@@ -1,33 +1,5 @@
 const envelope = document.getElementById("envelope");
-const mensagem = document.querySelector(".mensagem");
-const clique = document.querySelector(".click");
-
-let aberto = false;
 
 envelope.addEventListener("click", () => {
-
-    if (!aberto) {
-
-        envelope.classList.add("open");
-
-        clique.style.opacity = "0";
-
-        setTimeout(() => {
-            mensagem.classList.add("show");
-        }, 700);
-
-        aberto = true;
-
-    } else {
-
-        envelope.classList.remove("open");
-
-        mensagem.classList.remove("show");
-
-        clique.style.opacity = "1";
-
-        aberto = false;
-
-    }
-
+    envelope.classList.toggle("aberto");
 });
